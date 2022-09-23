f2f59becd8d2d0b9d7892f6f0f2bbdcf98b8e267
FISSO-VINCENZO\vince <vincenzosalvati@hotmail.it>
2022-09-23 16:15:10 +0200

    Renamed

--- bot/BotAI.py
+++ bot/BotAI.py
@@ -11,7 +11,7 @@ from chatterbot.response_selection import get_random_response
 from chatterbot.trainers import ListTrainer
 
 from utilities.Language import Language
-from utilities.util import extract_cities_from, greet_morning_in, greet_afternoon_in, greet_evening_in, \
+from utilities.utils import greet_morning_in, greet_afternoon_in, greet_evening_in, \
     greet_night_in, actually_is_morning, actually_is_afternoon, actually_is_evening, actually_is_night, \
     know_name_bot_from, know_time_from, know_date_from, \
     know_weather_from, exit_from
@@ -19,11 +19,15 @@ from utilities.util import extract_cities_from, greet_morning_in, greet_afternoo
 logging.basicConfig(level=logging.CRITICAL)
 
 
-class Bot:
+# noinspection PyShadowingNames
+class BotAI:
     bot = None
     name = ""
+    voice_and_recognition = None
+    language = ""
+    cities = None
 
-    def __init__(self, name):
+    def __init__(self, name, voice_and_recognition, cities):
         self.name = name
         self.bot = ChatBot(
             name,
@@ -35,10 +39,13 @@ class Bot:
             statement_comparison_function=levenshtein_distance,
             response_selection_method=get_random_response
         )
+        self.voice_and_recognition = voice_and_recognition
+        self.language = voice_and_recognition.speech_language
+        self.cities = cities
 
-    def train(self, language):
+    def train(self):
         # addestramento con esempio di comunicazione
-        if language == Language.ITALIANO.value:
+        if self.language == Language.ITALIANO.value:
             with open("./example_of_communication/comunicazione_di_addestramento") as f:
                 conversation = f.readlines()
                 trainer = ListTrainer(self.bot)
@@ -49,86 +56,82 @@ class Bot:
                 trainer = ListTrainer(self.bot)
                 trainer.train(conversation)
 
-    def get_name_bot(self, language):
-        if language == Language.ITALIANO.value:
+    def get_name_bot(self):
+        if self.language == Language.ITALIANO.value:
             return f"Mi chiamo {self.name}, ma tu puoi chiamarmi come vuoi!"
         else:
             return f"My name is {self.name}, but you can call me whatever you want!"
 
-    @staticmethod
-    def get_date(language):
-        def convert_number_to_word(language, month_number):
-            month_word = ""
-            if language == Language.ITALIANO.value:
+    def get_date(self):
+        def convert_number_to_word(month_number):
+            if self.language == Language.ITALIANO.value:
                 if month_number == "1" or month_number == "01":
-                    month_word = "Gennaio"
+                    return "Gennaio"
                 elif month_number == "2" or month_number == "02":
-                    month_word = "Febbraio"
+                    return "Febbraio"
                 elif month_number == "3" or month_number == "03":
-                    month_word = "Marzo"
+                    return "Marzo"
                 elif month_number == "4" or month_number == "04":
-                    month_word = "Aprile"
+                    return "Aprile"
                 elif month_number == "5" or month_number == "05":
-                    month_word = "Maggio"
+                    return "Maggio"
                 elif month_number == "6" or month_number == "06":
-                    month_word = "Giugno"
+                    return "Giugno"
                 elif month_number == "7" or month_number == "07":
-                    month_word = "Luglio"
+                    return "Luglio"
                 elif month_number == "8" or month_number == "08":
-                    month_word = "Agosto"
+                    return "Agosto"
                 elif month_number == "9" or month_number == "09":
-                    month_word = "Settembre"
+                    return "Settembre"
                 elif month_number == "10":
-                    month_word = "Ottobre"
+                    return "Ottobre"
                 elif month_number == "11":
-                    month_word = "Novembre"
+                    return "Novembre"
                 elif month_number == "12":
-                    month_word = "Dicembre"
+                    return "Dicembre"
             else:
                 if month_number == "1" or month_number == "01":
-                    month_word = "January"
+                    return "January"
                 elif month_number == "2" or month_number == "02":
-                    month_word = "February"
+                    return "February"
                 elif month_number == "3" or month_number == "03":
-                    month_word = "March"
+                    return "March"
                 elif month_number == "4" or month_number == "04":
-                    month_word = "April"
+                    return "April"
                 elif month_number == "5" or month_number == "05":
-                    month_word = "May"
+                    return "May"
                 elif month_number == "6" or month_number == "06":
-                    month_word = "June"
+                    return "June"
                 elif month_number == "7" or month_number == "07":
-                    month_word = "July"
+                    return "July"
                 elif month_number == "8" or month_number == "08":
-                    month_word = "August"
+                    return "August"
                 elif month_number == "9" or month_number == "09":
-                    month_word = "September"
+                    return "September"
                 elif month_number == "10":
-                    month_word = "October"
+                    return "October"
                 elif month_number == "11":
-                    month_word = "November"
+                    return "November"
                 elif month_number == "12":
-                    month_word = "December"
+                    return "December"
 
             return month_word
 
         def translate_english_to_italian_(english_day_word):
-            italian_day_word = ""
             if english_day_word == "Monday":
-                italian_day_word = "Lunedì"
+                return "Lunedì"
             elif english_day_word == "Tuesday":
-                italian_day_word = "Martedì"
+                return "Martedì"
             elif english_day_word == "Wednesday":
-                italian_day_word = "Mercoledì"
+                return "Mercoledì"
             elif english_day_word == "Thursday":
-                italian_day_word = "Giovedì"
+                return "Giovedì"
             elif english_day_word == "Friday":
-                italian_day_word = "Venerdì"
+                return "Venerdì"
             elif english_day_word == "Saturday":
-                italian_day_word = "Sabato"
+                return "Sabato"
             elif english_day_word == "Sunday":
-                italian_day_word = "Domenica"
-            return italian_day_word
+                return "Domenica"
 
         # preleva data e rimuovi gli zeri superflui
         if platform.system() == "Windows":
@@ -144,23 +147,22 @@ class Bot:
         year = splitted_date[2]
 
         # traduzione del giorno in italiano
-        if language == Language.ITALIANO.value:
+        if self.language == Language.ITALIANO.value:
             day_word = translate_english_to_italian_(day_word)
 
         # conversione del mese da numero a parola
-        month_word = convert_number_to_word(language, month_number)
+        month_word = convert_number_to_word(month_number)
 
         # formula la data
         current_date = day_word + " " + day_number + " " + month_word + " " + year
 
         # ritorna la data
-        if language == Language.ITALIANO.value:
+        if self.language == Language.ITALIANO.value:
             return f"Oggi è: {current_date}"
         else:
             return f"Today is: {current_date}"
 
-    @staticmethod
-    def get_time(language):
+    def get_time(self):
         # preleva l'orario e rimuovi gli zeri superflui
         if platform.system() == "Windows":
             current_time = datetime.now().strftime('%#H e %#M')
@@ -168,22 +170,21 @@ class Bot:
             current_time = datetime.now().strftime('%-H e %-M')
 
         # ritorna l'orario
-        if language == Language.ITALIANO.value:
+        if self.language == Language.ITALIANO.value:
             return f"Sono le ore: {current_time}"
         else:
             return f"Are the hours: {current_time}"
 
-    @staticmethod
-    async def get_weather(recognized_data, language):
+    # noinspection GrazieInspection
+    async def get_weather(self, recognized_data):
 
+        # noinspection GrazieInspection
         def get_city_from(recognized_data):
             # dividi la frase in parole
             words = recognized_data.split()
 
             # verifica se esiste il nome di una città nel comando
             if len(words) > 2:
-                # preleva la lista delle città da un file in formato csv
-                cities = extract_cities_from("./utilities/cities.csv")
 
                 # verifica se esiste il nome di una città con 4 parole
                 if len(words) >= 4:
@@ -193,7 +194,7 @@ class Bot:
                                         words[index_word + 2] + " " + \
                                         words[index_word + 3]
                         # verifica e ritorna la città
-                        if possible_city in cities:
+                        if possible_city in self.cities:
                             return True, possible_city
 
                 # verifica se esiste il nome di una città con 3 parole
@@ -203,7 +204,7 @@ class Bot:
                                         words[index_word + 1] + " " + \
                                         words[index_word + 2]
                         # verifica e ritorna la città
-                        if possible_city in cities:
+                        if possible_city in self.cities:
                             return True, possible_city
 
                 # verifica se esiste il nome di una città con 2 parole
@@ -212,16 +213,16 @@ class Bot:
                         possible_city = words[index_word] + " " + \
                                         words[index_word + 1]
                         # verifica e ritorna la città
-                        if possible_city in cities:
+                        if possible_city in self.cities:
                             return True, possible_city
 
                 # verifica se esiste il nome di una città con 1 parola
                 for possible_city in words:
-                    if possible_city in cities:
+                    if possible_city in self.cities:
                         return True, possible_city
 
             # ritorna città non trovata
-            if language == Language.ITALIANO.value:
+            if self.language == Language.ITALIANO.value:
                 return False, "Mi spiace. Non ho capito il nome della città."
             else:
                 return False, "I'm sorry. I didn't get the name of the city."
@@ -246,17 +247,21 @@ class Bot:
                     forecast_three_days += str(forecast.date) + "\n"
                     forecast_three_days += str(forecast.astronomy) + "\n"
                     for hourly in forecast.hourly:
-                        if language == Language.ITALIANO.value:
-                            forecast_three_days += f" --> ora: {hourly.time / 100} temperatura: {(hourly.temperature)}C° {hourly.description} {hourly.type!r}\n"
+                        if self.language == Language.ITALIANO.value:
+                            forecast_three_days += f" --> ora: {hourly.time / 100} temperatura: " \
+                                                   f"{hourly.temperature}C° {hourly.description} {hourly.type!r}\n"
                         else:
-                            forecast_three_days += f" --> hour: {hourly.time / 100} temperature: {(hourly.temperature)}C° {hourly.description} {hourly.type!r}\n"
+                            forecast_three_days += f" --> hour: {hourly.time / 100} temperature: " \
+                                                   f"{hourly.temperature}C° {hourly.description} {hourly.type!r}\n"
                 print(city + "\n" + forecast_three_days)
 
                 # ritorna informazioni meteorologiche e astronomiche della città
-                if language == Language.ITALIANO.value:
-                    return "Attualmente a " + city + " ci sono " + current_temperature + " gradi centigradi. Ti mostro le previsioni nei prossimi 2 giorni e i dettagli astronomici."
+                if self.language == Language.ITALIANO.value:
+                    return "Attualmente a " + city + " ci sono " + current_temperature + \
+                           " gradi centigradi. Ti mostro le previsioni nei prossimi 2 giorni e i dettagli astronomici."
                 else:
-                    return "Currently at " + city + " there are " + current_temperature + "degrees centigrade. I show you the forecast over the next 2 days and astronomical details."
+                    return "Currently at " + city + " there are " + current_temperature + \
+                           "degrees centigrade. I show you the forecast over the next 2 days and astronomical details."
 
             # città non rilevata
             else:
@@ -311,52 +316,51 @@ class Bot:
         # ritorna cosa è stato rilevato
         return is_greeting, correct_greeting
 
-    @staticmethod
-    def get_correct_greeting_in(language):
+    def get_correct_greeting_in(self):
         # ritorna la correzione da parte del bot
         if actually_is_morning:
-            if language == Language.ITALIANO.value:
+            if self.language == Language.ITALIANO.value:
                 return "Sono le " + str(datetime.now().hour) + " del mattino. Buon giorno!"
             else:
                 return "It is " + str(datetime.now().hour) + " in the morning. Good morning!"
         elif actually_is_afternoon:
-            if language == Language.ITALIANO.value:
+            if self.language == Language.ITALIANO.value:
                 return "Sono le " + str(datetime.now().hour) + " del pomeriggio. Buon pomeriggio!"
             else:
                 return "It is " + str(datetime.now().hour) + " in the afternoon. Good afternoon!"
         elif actually_is_evening:
-            if language == Language.ITALIANO.value:
+            if self.language == Language.ITALIANO.value:
                 return "Sono le " + str(datetime.now().hour) + " di sera. Buona sera!"
             else:
                 return "It is " + str(datetime.now().hour) + " in the evening. Good evening!"
         else:
-            if language == Language.ITALIANO.value:
+            if self.language == Language.ITALIANO.value:
                 return "Sono le " + str(datetime.now().hour) + " di notte. Buona notte!"
             else:
                 return "It is " + str(datetime.now().hour) + " at night. Good night!"
 
-    def get_response(self, recognized_data, language):
+    def get_response(self, recognized_data):
         # esci
         if exit_from(recognized_data):
             return None
 
         # restituisci nome bot
         elif know_name_bot_from(recognized_data):
-            return self.get_name_bot(language)
+            return self.get_name_bot()
 
         # restituisci orario
         elif know_time_from(recognized_data):
-            return self.get_time(language)
+            return self.get_time()
 
         # restituisci data
         elif know_date_from(recognized_data):
-            return self.get_date(language)
+            return self.get_date()
 
         # restituisci meteo
         elif know_weather_from(recognized_data):
             if os.name == "nt":
                 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-            return asyncio.run(self.get_weather(recognized_data, language))
+            return asyncio.run(self.get_weather(recognized_data))
 
         # restituisci risposta data dall'addestramento e verifica un eventuale saluto
         else:
@@ -370,7 +374,7 @@ class Bot:
             if is_greeting:
                 # saluto non appropriato
                 if not correct_greeting:
-                    response = self.get_correct_greeting_in(language)
+                    response = self.get_correct_greeting_in()
             else:
 
                 # verifica l'eventuale saluto da parte del bot
@@ -384,4 +388,4 @@ class Bot:
                         break
 
             # ritorna la risposta
-            return str(response)
+            return response.encode(encoding="ascii", errors="ignore").decode()
